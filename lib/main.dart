cf74a5d11ff848c072b61818990f957f5705fb5f
cparragatorres <humbertpt14@gmail.com>
2022-05-21 19:30:43 -0500

    Funcionamiento del botón Verdadero

--- lib/main.dart
+++ lib/main.dart
@@ -15,13 +15,37 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class QuizPage extends StatelessWidget {
-  const QuizPage({Key? key}) : super(key: key);
+class QuizPage extends StatefulWidget {
+
+  @override
+  State<QuizPage> createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  List<String>questions = [
+    "El hombre llegó a la luna?",
+    "La tierra es plana?",
+    "Desayunaron?",
+  ];
+
+  List<bool>answer = [
+    false,
+    false,
+    true,
+  ];
+
+  int questionNumber = 0;
+
+  List<Icon> scoreKeeper = [
+
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xff272c2f),
       appBar: AppBar(
+        backgroundColor: Color(0xff272c2f),
         title: Text("QuizApp"),
         centerTitle: true,
       ),
@@ -29,21 +53,49 @@ class QuizPage extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Expanded(
-              flex: 5,
-              child: Text("Aqui va la pregunta")),
-          Expanded(
-            child: MaterialButton(
-              color: Colors.greenAccent,
-              child: Text("Verdadero"),
-              onPressed: () {},
+            flex: 5,
+            child: Center(
+              child: Text(
+                questions[questionNumber],
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 24.0, color: Colors.white),
+              ),
             ),
           ),
           Expanded(
-            child: MaterialButton(
-              color: Colors.redAccent,
-              child: Text("Falso"),
-              onPressed: () {},
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: MaterialButton(
+                color: const Color(0xff00e187),
+                child: const Text("Verdadero"),
+                onPressed: () {
+
+                  bool correctAnswer = answer[questionNumber];
+                  if(correctAnswer == true){
+                    scoreKeeper.add(Icon(Icons.check, color: const Color(0xff00e187),),);
+                  }else {
+                    scoreKeeper.add(Icon(Icons.close, color: const Color(0xfff62d63),),);
+                  }
+                  setState((){
+
+                  });
+
+                },
+              ),
             ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: MaterialButton(
+                color: const Color(0xfff62d63),
+                child: const Text("Falso"),
+                onPressed: () {},
+              ),
+            ),
+          ),
+          Row(
+            children: scoreKeeper,
           ),
         ],
       ),
